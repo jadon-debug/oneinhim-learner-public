e6a1b682a7ecd6d900fd193aff65bdd79a51b8ba
Jadon Calvert <jadoncalvert@users.noreply.github.com>
2026-05-23 09:08:29 +0200

    Deploy learner app

---
+++ oneinhim_team_sync_config.js
@@ -0,0 +1,9 @@
+window.ONEINHIM_TEAM_SYNC = {
+  enabled: true,
+  provider: "supabase",
+  supabaseUrl: "https://jlrgfcadmaoigybvzdsx.supabase.co",
+  supabaseAnonKey: "sb_publishable_zzOFELTx0tdMQhwVT6xsmQ_1uG_ahos",
+  tableName: "oih_content_packages"
+};
+
+window.OIH_TEAM_SYNC_CONFIG = window.ONEINHIM_TEAM_SYNC;
